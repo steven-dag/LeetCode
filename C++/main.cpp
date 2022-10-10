e5497eaea4aebc5986083a00d94c628daa03b0ef
steven-dag <88541354+steven-dag@users.noreply.github.com>
2022-10-10 14:14:52 +0200

    Update main.cpp

--- C++/main.cpp
+++ C++/main.cpp
@@ -9,7 +9,14 @@ int main()
 {
   LeetCode leet;
   
+  // Max product call
   vector<int> nums = {3,4,5,2};
   cout << leet.maxProduct(nums);
+  
+  // Add spaces call
+  string s="LeetcodeHelpsMeLearn";
+  vector<int> spaces = {8,13,15};
+
+  cout << leet.addSpaces(s, spaces);
   return 0;
 }
