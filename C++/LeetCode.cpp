c9116914a11f8ddc586cdd50537a3052b6c6a633
steven-dag <88541354+steven-dag@users.noreply.github.com>
2022-10-10 14:13:39 +0200

    Update LeetCode.cpp
    Added the implementation for addSpaces

--- C++/LeetCode.cpp
+++ C++/LeetCode.cpp
@@ -4,3 +4,20 @@ int LeetCode::maxProduct(vector<int>& nums){
     sort(nums.begin(), nums.end(), greater<int>());
     return (nums[0]-1)*(nums[1]-1);
 }
+
+
+string LeetCode::addSpaces(string s, vector<int>& spaces)
+{
+    string ans;
+    int j = 0;
+
+    for (int i=0; i < s.length(); i++)
+    {
+        if (spaces[j] == i && j < spaces.size())
+        {
+            ans += ' ';
+            j++;
+        }
+        ans += s[i];
+    }
+    return ans;
