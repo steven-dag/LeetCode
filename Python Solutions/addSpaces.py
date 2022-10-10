24f883fdcfee506005e398fdaab50f06fb9774e8
steven-dag <88541354+steven-dag@users.noreply.github.com>
2022-10-10 14:12:38 +0200

    Update addSpaces.py
    *Added another implementation of addSpaces, works fine for smaller strings. Causes Time Limit Exceeded for too long strings

--- Python Solutions/addSpaces.py
+++ Python Solutions/addSpaces.py
@@ -15,7 +15,13 @@ class Solution:
         ans.append(s[space:])
         # Join everything using " " seperator
         return " ".join(ans)
-
+    
+    def addSpaces_v2(self, s: str, spaces: List[int]) -> str:
+        ans = s
+		for i in range(len(spaces)-1, -1, -1):
+			ans = ans[0:spaces[i]] + ' ' + ans[spaces[i]:]
+		return ans
+    
 
 if __name__ == '__main__':
     a = Solution().maxProduct([10, 2, 5, 2])
