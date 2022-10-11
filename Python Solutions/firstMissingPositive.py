7623c8a78717530819f719793982c3b1801cc8a0
steven-dag <88541354+steven-dag@users.noreply.github.com>
2022-10-11 20:02:13 +0200

    Update firstMissingPositive.py

--- Python Solutions/firstMissingPositive.py
+++ Python Solutions/firstMissingPositive.py
@@ -8,12 +8,6 @@ class Solution:
 
         for num in nums:
             if curr_min_num == num:
-                # This means that curr_min_num already exists in num, so it cant be the smallest
-                # missing positive number
-                # ex. num = [1, 2, 0] -> sorted: [0, 1, 2]
-                # At the first number we'll have curr_min_num = 1 == num
-                # At the third loop we'll end up at curr_min_num = 2 == num
-                # Which when we exit the loop we'll have curr_min_num = 3 which is the answer
                 curr_min_num += 1
             elif nums[0] > 0:
                 return curr_min_num
